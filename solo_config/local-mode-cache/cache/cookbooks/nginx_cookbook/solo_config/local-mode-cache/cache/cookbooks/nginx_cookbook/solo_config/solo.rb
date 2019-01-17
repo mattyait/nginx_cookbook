e62cdb0bcb36fc2425cf8c35c27b0ee02afa8c97
root <root@ip-172-31-44-84.ec2.internal>
2019-01-17 05:14:22 +0000

    removed local-mode-cache folder

--- solo_config/local-mode-cache/cache/cookbooks/nginx_cookbook/solo_config/local-mode-cache/cache/cookbooks/nginx_cookbook/solo_config/solo.rb
+++
@@ -1,7 +0,0 @@
-#cookbook_path '/var/chef/cookbooks'
-cookbook_path [
-               '/var/chef/cookbooks',
-               '/var/chef/cookbooks/nginx_cookbook/berks-cookbooks'
-              ]
-data_bag_path '/var/chef/data_bags'
-node_name 'matty'
